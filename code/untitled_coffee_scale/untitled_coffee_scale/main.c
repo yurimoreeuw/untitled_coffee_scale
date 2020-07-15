12ad4bed087703be528b56cda5b4e6508d20164e
yuri <yuri.moreeuw@gmail.com>
2020-07-14 22:10:29 -0300

    created a project on atmelstudio

---
+++ code/untitled_coffee_scale/untitled_coffee_scale/main.c
@@ -0,0 +1,18 @@
+/*
+ * untitled_coffee_scale.c
+ *
+ * Created: 14/07/2020 22:08:46
+ * Author : Yuri
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
